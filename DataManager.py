f0fe311b9a1bcd7f00d927db7605359221e78749
sci-web <sci-web@protonmail.com>
2017-08-24 18:10:04 +0200

    Parser for PubMed XMLs to MongoDB

---
+++ DataManager.py
@@ -0,0 +1,61 @@
+from pymongo import MongoClient
+
+
+class DBcall(object):
+    SECRET_KEY = 'devil in the sky'
+    DATABASE = MongoClient()['sci-web']
+
+    def __init__(self, collection, pid):
+        """
+        infile (list): lines from file
+        fields (dict): the columns of the CSV file to load like {num : name}
+        """
+        self.client = self.DATABASE[collection]
+        self.pid = pid
+        # self.grid = gridfs.GridFS(MongoClient()['sci-web'])
+
+    def loadData(self, data):
+        for doc in data:
+            try:
+                self.client.insert_one(doc)
+            except Exception, e:
+                action = "Load to DB failed: " + str(e)
+                putLog(action, self.pid, "DB load", "exception")
+        # self.client.insert_many(data)
+
+    def updateData(self, keys, values):
+        try:
+            self.client.update_one(keys, {'$set': values})
+        except Exception, e:
+            action = "Update DB record failed: " + str(e)
+            putLog(action, self.pid, "DB record update", "exception")
+
+    def updateDatapull(self, keys, values):
+        try:
+            self.client.update_one(keys, {'$addToSet': values})
+        except Exception, e:
+            action = "Update DB record failed: " + str(e)
+            putLog(action, self.pid, "DB record update", "exception")
+
+    def findData(self, keys):
+        return self.client.find_one(keys)
+
+    def Distinct(self, field, keys):
+        return self.client.distinct(field, keys)
+
+    def findDatalist(self, keys):
+        return self.client.find(keys)
+
+    def findDatalistSorted(self, keys, sort, d):
+        return self.client.find(keys).sort(sort, d)
+
+    def findDatalist_by_a_key(self, key, lst):
+        return self.client.find({key: {'$in': lst}})
+
+    def findDatalist_by_comp_keys(self, keys):
+        return self.client.find({'$or': keys})
+
+
+def putLog(action, pid, task, tp):
+    logdata = {"time": datetime.now(), "pid": pid, "action": action, "task": task, "type": tp}
+    DBcall("logs", pid).loadData([logdata])
---
+++ GetParseXML.py
@@ -0,0 +1,119 @@
+# encoding=utf8
+import sys
+reload(sys)
+sys.setdefaultencoding('utf8')
+import re
+import os
+import xml.etree.ElementTree as etree
+from DataManager import DBcall, putLog
+from datetime import datetime
+from os import listdir
+from os.path import isfile, join
+import gzip
+import shutil
+
+xmLpath = "./PubMed/ftp.ncbi.nlm.nih.gov/pubmed/baseline/"
+gzips = [f for f in listdir(xmLpath) if isfile(join(xmLpath, f)) and f[-3:] == ".gz"]
+
+# get our big file by chunks to process it serially:
+# name space for author & paper:
+names_a = {"LastName": "last_name", "ForeName": "first_name", "Initials": "initials", "ORCID": "orcid", "Affiliation": "af_place", "Country": "af_country"}
+names_p = {"ArticleTitle": "title", "AbstractText": "abstract", "ISSN": "issn", "Title": "journal"}
+months = {"Jan": 1, "Feb": 2, "Mar": 3, "Apr": 4, "May": 5, "Jun": 6, "Jul": 7, "Aug": 8, "Sep": 9, "Oct": 10, "Nov": 11, "Dec": 12}
+print "start unzip and loading all gzip files one by one: " + str(datetime.now())
+for gz in sorted(gzips):
+    xmL = xmLpath + gz
+    print xmL
+    with gzip.open(xmL, 'rb') as f:
+        xmL = xmL[:-3]
+        with open(xmL, "w") as ff:
+            ff.write(f.read())
+    data = {}
+    names = []
+    pdate = []
+    a_data = {}
+    af_data = {}
+    p_data = {}
+    ca_data = {}
+    ca_list = []
+    kw_list = []
+    n = 0   # line number to catch parcing/loading bugs
+    print "start loading " + xmL + " : " + str(datetime.now())
+    for event, elem in etree.iterparse(xmL, events=('start', 'end', 'start-ns', 'end-ns')):
+        childs = {"Year": "1000", "Month": "1", "Day": "1"}
+        memo = {}
+        if event == 'start':
+            date = ""
+            for child in elem:
+                if child.text:
+                    childs[child.tag] = child.text
+                if len(childs) == 0:
+                    if hasattr(elem, 'getroot'):
+                        elem = elem.getroot()
+            for k, v in names_a.iteritems():
+                if k == elem.tag:
+                    a_data[v] = elem.text
+                    if k == "Affiliation" and elem.text is not None:
+                        words = elem.text.split(" ")
+                        for w in words:
+                            if re.search(r'\w+\@\w+\.\w+', w):
+                                if re.search(r'\.$', w):
+                                    w = w[:-1]
+                                a_data["email"] = w
+
+            for k, v in names_p.iteritems():
+                if k == elem.tag:
+                    p_data[v] = elem.text
+            if elem.tag == "PubDate":
+                month = childs["Month"]
+                if not re.match("\d", month):
+                    month = months[childs["Month"]]
+                date = datetime.strptime(childs["Year"] + "-" + str(month) + "-" + childs["Day"], "%Y-%m-%d")
+                p_data["pub_date"] = date
+
+            for k, v in elem.attrib.iteritems():
+                if k == "PubStatus":
+                    date = datetime.strptime(childs["Year"] + "-" + childs["Month"] + "-" + childs["Day"], "%Y-%m-%d")
+                if k == "PubStatus" and v == "received":
+                    p_data["received_date"] = date
+                if k == "PubStatus" and v == "accepted":
+                    p_data["accepted_date"] = date
+                if k == "IdType" and v == "pubmed":
+                    p_data["pubmed"] = elem.text
+                if k == "IdType" and v == "doi":
+                    p_data["doi"] = elem.text
+                if k == "Source" and v == "ORCID":
+                    if re.search('http://orcid.org', elem.text):
+                        a_data["orcid"] = elem.text
+                    elif re.search('-', elem.text) and not re.search('http://orcid.org', elem.text):
+                        orcid = 'http://orcid.org/' + elem.text
+                    else:
+                        orcid = 'http://orcid.org/' + elem.text[0:4] + "-" + elem.text[4:8] + "-" + elem.text[8:12] + "-" + elem.text[12:16]
+                p_data["num_of_authors"] = len(ca_list)
+                data["paper"] = p_data
+            if elem.tag == "Keyword":
+                kw_list.append(elem.text)
+                # print event, elem.tag, elem.attrib, '>', elem.text, '<', date  #, json
+        if elem.tag == 'Author' and event == 'end':
+            a_data["position"] = len(ca_list) + 1
+            ca_list.append(a_data)
+            a_data = {}
+        if elem.tag == 'PubmedArticle' and event == 'end':
+            if len(ca_list) > 0:
+                data["author"] = ca_list[0]
+            ca_list = ca_list[1:]
+            data["coauthors"] = ca_list
+            data["keywords"] = kw_list
+            DBcall("papers", n).loadData([data])
+            # for k, v in data.iteritems():
+            #     print k, v
+            af_data = {}
+            p_data = {}
+            data = {}
+            ca_list = []
+            kw_list = []
+        n += 1
+    print "end loading " + xmL + " : " + str(datetime.now())
+    with open(xmL, 'rb') as f_in, gzip.open(xmL + '.gz', 'wb') as f_out:
+        shutil.copyfileobj(f_in, f_out)
+    os.unlink(xmL)
