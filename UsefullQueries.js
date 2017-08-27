bd52c9b72b5d6aad6caa82f196c4970e300e1257
sci-web <sci-web@protonmail.com>
2017-08-27 22:12:20 +0200

    Loader parsing event corrected, plotly random graph vis

--- UsefullQueries.js
+++ UsefullQueries.js
@@ -1,1 +1,9 @@
 db.papers.find({authors: {$elemMatch: {"last_name":"Ezaki", "initials":"H"}}},{pubmed:1,authors:1})
+
+db.papers.ensureIndex({pubmed:1})
+db.papers.ensureIndex({pub_date:1})
+db.papers.ensureIndex({xml_source:1})
+db.papers.ensureIndex({received_date:1})
+db.papers.ensureIndex({title:1})
+db.papers.ensureIndex({doi:1})
+db.papers.ensureIndex({keywords:1})
