2ca7cd5dd05dc876131ad15fdafa7831d8df2b70
sci-web <sci-web@protonmail.com>
2017-08-29 20:15:34 +0200

    Dates format corrected, authors collection: all pubs to their author

--- GetParseXML_correct_dates.py
+++ GetParseXML_correct_dates.py
@@ -23,7 +23,7 @@ names_p = {"ArticleTitle": "title", "AbstractText": "abstract", "ISSN": "issn", 
 months = {"Jan": 1, "Feb": 2, "Mar": 3, "Apr": 4, "May": 5, "Jun": 6, "Jul": 7, "Aug": 8, "Sep": 9, "Oct": 10, "Nov": 11, "Dec": 12}
 print str(datetime.now())[:-7] + " : start unzip and loading all gzip files one by one"
 nn = 0
-for gz in sorted(gzips)[875:]:
+for gz in sorted(gzips)[80:]:
     xmL = xmLpath + gz
     with gzip.open(xmL, 'rb') as f:
         xmL = xmL[:-3]
@@ -34,14 +34,12 @@ for gz in sorted(gzips)[875:]:
                 print "failed to unpack " + gz + "exception: " + e
                 continue
     data = {}
-    ca_list = []
-    kw_list = []
-    a_data = {}
     n = 0   # line number to catch parcing/loading bugs
-    print str(datetime.now())[:-7] + " : starting to load " + xmL
+    print str(datetime.now())[:-7] + " : starting to update " + xmL
     for event, elem in etree.iterparse(xmL, events=('start', 'end', 'start-ns', 'end-ns')):
         childs = {"Year": "1000", "Month": "1", "Day": "1"}
         # take fully populated event (i.e. event != start! )
+        pubmed = 0
         if event == 'end':
             date = ""
             for child in elem:
@@ -50,31 +48,9 @@ for gz in sorted(gzips)[875:]:
                 if len(childs) == 0:
                     if hasattr(elem, 'getroot'):
                         elem = elem.getroot()
-            for k, v in names_a.iteritems():
-                if k == elem.tag:
-                    a_data[v] = elem.text
-                    if k == "Affiliation" and elem.text is not None:
-                        words = elem.text.split(" ")
-                        for w in words:
-                            if re.search(r'\w+\@\w+\.\w+', w):
-                                if re.search(r'\.$', w):
-                                    w = w[:-1]
-                                a_data["email"] = w
-
             for k, v in names_p.iteritems():
                 if k == elem.tag:
                     data[v] = elem.text
-            if elem.tag == "PubDate":
-                month = childs["Month"]
-                if not re.match("\d", month):
-                    month = months[childs["Month"]]
-                    try:
-                        date = datetime.strptime(childs["Year"] + "-" + str(month) + "-" + childs["Day"], "%Y-%m-%d")
-                    except:
-                        day = int(childs["Day"]) - 2
-                        date = datetime.strptime(childs["Year"] + "-" + str(month) + "-" + str(day), "%Y-%m-%d")
-                        putLog("Date format was wrong for pid (pid = records number), corrected for -2 days", n, "XML parcing" + gz, "format")
-                data["pub_date"] = date
             for k, v in elem.attrib.iteritems():
                 if k == "PubStatus":
                     try:
@@ -90,37 +66,14 @@ for gz in sorted(gzips)[875:]:
                 if k == "IdType" and v == "pubmed" and elem.tag == "ArticleId":
                     data["pubmed"] = 0
                     if elem.text is not None:
-                        data["pubmed"] = int(elem.text)
-                if k == "IdType" and v == "doi" and elem.tag == "ArticleId":
-                    data["doi"] = elem.text
-                if k == "Source" and v == "ORCID" and elem.tag == "Identifier":
-                    if re.search('http://orcid.org', elem.text):
-                        a_data["orcid"] = elem.text
-                    elif re.search('-', elem.text) and not re.search('http://orcid.org', elem.text):
-                        a_data["orcid"] = 'http://orcid.org/' + elem.text
-                    else:
-                        a_data["orcid"] = 'http://orcid.org/' + elem.text[0:4] + "-" + elem.text[4:8] + "-" + elem.text[8:12] + "-" + elem.text[12:16]
-                data["num_of_authors"] = len(ca_list)
-            if elem.tag == "Keyword":
-                kw_list.append(elem.text)
-                # print event, elem.tag, elem.attrib, '>', elem.text, '<', date  #, json
-        if elem.tag == 'Author' and event == 'end':
-            a_data["position"] = len(ca_list) + 1
-            ca_list.append(a_data)
-            a_data = {}
+                        pubmed = int(elem.text)
         if elem.tag == 'PubmedArticle' and event == 'end':
-            data["authors"] = ca_list
-            data["keywords"] = kw_list
-            data["xml_source"] = gz
-            DBcall("papers", n).loadData([data])
-            p_data = {}
+            keys = {"pubmed": pubmed}
+            DBcall("papers", n).updateData(keys, data)
             data = {}
-            ca_list = []
-            kw_list = []
+            pubmed = 0
             n += 1
     nn += 1
-    print str(datetime.now())[:-7] + " : finished loading " + str(nn) + "th " + xmL
-    # with open(xmL, 'rb') as f_in, gzip.open(xmL + '.gz', 'wb') as f_out:
-    #     shutil.copyfileobj(f_in, f_out)
+    print str(datetime.now())[:-7] + " : finished updateing " + str(nn) + "th " + xmL
     os.unlink(xmL)
 print str(datetime.now())[:-7] + " : done"
--- MakeGraph.py
+++ MakeGraph.py
@@ -1,9 +1,17 @@
 # encoding=utf8
 import sys
 import networkx as nx
+import random
 from DataManager import DBcall
 from datetime import datetime
-import matplotlib.pyplot as plt
+import operator
+import math
+# import plotly.plotly as py
+import plotly
+from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
+from plotly.graph_objs import *
+# from plotly.graph_objs import Scatter, Figure, Layout
+
 reload(sys)
 sys.setdefaultencoding('utf8')
 
@@ -19,17 +27,16 @@ def idAuthor(papers, flds):
                 try:
                     pubs_to_author[name]
                 except:
-                    a_pubs = authorPubs(d, pubmed, papers, name, flds)
+                    # author papers with author positions
+                    a_pubs = authorPubs(d, pubmed, papers, name, flds, d["position"], len(authors))
                     for k, v in a_pubs.iteritems():
                         if len(v) > 0:
                             # print k, sorted(v, key=int)
-                            pubs_to_author[k] = sorted(v, key=int)
-            else:
-                print pubmed
+                            pubs_to_author[k] = sorted(v)
     return pubs_to_author
 
 
-def authorPubs(d, pubmed, papers, name, flds):
+def authorPubs(d, pubmed, papers, name, flds, pos, coauthors):
     pubs = []
     for pm, authors in papers.iteritems():
         # check out the author in all other than a given pubmed paper
@@ -50,11 +57,11 @@ def authorPubs(d, pubmed, papers, name, flds):
                     # (if not the same author in another paper)
                     if d[f] != dd[f] and dd[f] != "" and d[f] != "":
                         def_id += 1
-                # def_id = 0 if the same author, including with undefined fields:
+                # def_id = 0 if the same author, including with undef fields:
                 if def_id == 0 and stringName(dd, flds) != "":
-                    pubs.append(pm)
+                    pubs.append([pm, dd["position"], len(authors)])
     # if len(pubs) > 0:   # remove this condition if authors with < 2 papers
-    pubs.append(pubmed)
+    pubs.append([pubmed, pos, coauthors])
     return {str(name): pubs}
 
 
@@ -74,40 +81,93 @@ def stringName(d, flds):
     if len(found) > 1:
         return ";".join(uname)
     else:
-        print d
+        print(d)
         return ""
 
 
+def AuthorsToPubs(adata, flds):
+    a_to_pubs = {}
+    print(len(adata.keys()))
+    # put author data in the DB with assigned id and list of papers
+    authors_w_p = {}
+    for num in range(len(adata.keys())):
+        au = adata.keys()[num]
+        adata_nums = {}
+        #  starting from 1 billion
+        a_id = 1000000000 + num + 1
+        adata_nums["assigned_id"] = a_id
+        for f in range(len(flds)):
+            adata_nums[flds[f]] = adata.keys()[num].split(";")[f]
+        p_list = []
+        for pps in adata[au]:
+            pubmed = pps[0]
+            position = pps[1]
+            coauthors = pps[2]
+            p_list.append({"pubmed": pubmed, "position": position, "coauthors": coauthors})
+            try:
+                a_to_pubs[pubmed].append(adata_nums["assigned_id"])
+            except:
+                a_to_pubs[pubmed] = [adata_nums["assigned_id"]]
+        adata_nums["papers"] = p_list
+        authors_w_p[a_id] = adata[au]
+        # print num, adata.keys()[num], adata[ adata.keys()[num] ]
+        # to link all co-authors with assigned id in the paper:
+
+        DBcall("authors", num).loadData([adata_nums])
+        # print num + 1, adata[au]
+    return a_to_pubs, authors_w_p
+
+
+def MakeGraphData(a_to_pubs):
+    keys = {"assigned_id": {'$gt': 0}}
+    authors = DBcall("authors", 0).findDatalist(keys)
+    e_tuples = {}
+    for au in authors:
+        # print nm, pbs
+        for p in au["papers"]:
+            all_a = a_to_pubs[p["pubmed"]]
+            for a in all_a:
+                if a != au["assigned_id"]:
+                    try:
+                        e_tuples[a, au["assigned_id"]] += 1
+                    except:
+                        e_tuples[a, au["assigned_id"]] = 1
+    return e_tuples
+
+
 def main():
-    G = nx.Graph()
-    flds = ["first_name", "last_name", "initials", "af_place", "af_country"]
-    # papers = DBcall("papers", 0).findDatalistSorted(keys, "pubmed", 1).limit(1000)
-    print str(datetime.now())[:-7] + " : extract data"
+    flds = ["first_name", "last_name", "initials", "af_place", "af_country", "orcid", "email"]
+    print(str(datetime.now())[:-7] + " : extract data")
     keys = {"pubmed": {'$gt': 0}}
-    papers = DBcall("papers", 0).findDatalist(keys).limit(100)
-    sp = {}
+    # or findDatalistSorted(keys, "pubmed", 1).limit(1000)
+    papers = DBcall("papers", 0).findDatalist(keys).limit(10000)
+    authors_to_pubmed = {}
     pps = []
     for p in papers:
         pps.append(p["pubmed"])
         if len(p["authors"]) > 0:
-            sp[ p["pubmed"] ] = p["authors"]
+            authors_to_pubmed[p["pubmed"]] = p["authors"]
+            # positions = []
+            # for p_a in p["authors"]:
+            #     positions.append(p_a["positions"])
     # authors with their papers (more than 1 paper):
-    print str(datetime.now())[:-7] + " : start data linking"
-    pubs_to_author = idAuthor(sp, flds)
+    print(str(datetime.now())[:-7] + " : start data linking")
+    pubs_to_author = idAuthor(authors_to_pubmed, flds)
     """
     we might have got here a list of authors where some has different fields,
     but the same firs_name, last_name, initials:
-    merge authors by longest metadata if they are met under the same name
-    (first, last name, initials) in the same paper list (name + affiliation):
+    so let's merge authors by longest metadata if they are met under the same
+    name (first, last, initials) in the same paper list (name+affiliation):
     the same metadata also can be RW differently being in different encoding
     """
     edges = []
     adata = dict(pubs_to_author)
-    print len(pubs_to_author.keys())
+    print(len(pubs_to_author.keys()))
     for nm, pbs in pubs_to_author.iteritems():
         for p in pbs:
             for c_nm, c_pbs in pubs_to_author.iteritems():
-                # if in the same paper in a list of paper assigned to another author...
+                # if the same p in this list of pprs assigned to
+                # another author...
                 if p in c_pbs and nm != c_nm:
                     # if metadata are different but name is the same:
                     nm_lst = nm.split(";")[:2]
@@ -123,64 +183,29 @@ def main():
                                 del adata[nm]
                             except:
                                 continue
-    a_to_pubs = {}
-    print len(adata.keys())
-    # put author data in the DB with assigned id and list of papers
-    for num in range(len(adata.keys())):
-        adata_nums = {}
-        adata_nums["assigned_id"] = num + 1
-        meta = {}
-        for f in range(len(flds)):
-            meta[flds[f]] = adata.keys()[num].split(";")[f]
-        adata_nums["meta"] = meta
-        adata_nums["papers"] = adata[ adata.keys()[num] ]
-        # print num, adata.keys()[num], adata[ adata.keys()[num] ]
-        # to link all co-authors with assigned id in the paper:
-        for p in adata[ adata.keys()[num] ]:
-            try:
-                a_to_pubs[p].append(adata_nums["assigned_id"])
-            except:
-                a_to_pubs[p] = [adata_nums["assigned_id"]]
 
-        DBcall("authors", num).loadData([adata_nums])
+    a_to_pubs, authors_w_p = AuthorsToPubs(adata, flds)
 
-    print list(set(pubs_to_author.keys()) - set(adata.keys()))
+    print(list(set(pubs_to_author.keys()) - set(adata.keys())))
 
-    keys = {"assigned_id": {'$gt': 0}}
-    authors = DBcall("authors", 0).findDatalist(keys)
-    e_tuples = {}
-    for au in authors:
-        # print nm, pbs
-        for p in au["papers"]:
-            all_a = a_to_pubs[p]
-            for a in all_a:
-                if a != au:
-                    try:
-                        e_tuples[a, au["assigned_id"]] += 1
-                    except:
-                        e_tuples[a, au["assigned_id"]] = 1
+    e_tuples = MakeGraphData(a_to_pubs)
+    count_n = len(e_tuples.keys())
+    aus = len(authors_w_p.keys())
+    print(str(datetime.now())[:-7] + " : " + str(aus) + " authors")
+    print(str(datetime.now())[:-7] + " : end linking")
 
+    G = nx.Graph()
+    nodes = {}
     for node, weight in e_tuples.iteritems():
         n1, n2 = node
-        G.add_nodes_from(node)
+        for nn in node:
+            try:
+                nodes[nn]
+            except:
+                G.add_node(nn)
+                nodes[nn] = 1
         G.add_edge(n1, n2, weight=e_tuples[node])
-    print "components: "
-    print nx.connected_components(G)
-    print "degree: "
-    print nx.degree(G)
-    # draw with node ids:
-    nx.draw_networkx(G)
-    # pos = nx.spring_layout(G)
-    # nx.draw_networkx_nodes(G, pos=pos, nodelist=G.nodes())
-    # nx.draw_networkx_edges(G, pos=pos, edgelist=G.edges())
-    plt.show()
-    # print list(set(sp.keys()) - set(a_to_pubs.keys()))
-    # for pp, au in a_to_pubs.iteritems():
-    #     print pp, au
-    print str(datetime.now())[:-7] + " : " + str(len(a_to_pubs.keys())) + " authors adding:"
-    print str(datetime.now())[:-7] + " : end linking"
-# def makeEdges(papers):
-#     for p in papers:
+
 
 if __name__ == '__main__':
     main()
